1af263f5d2090a3c6e9838708d3d23a3ad78849d
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-11-29 11:53:37 +0530

    :art: provider package; set stream on myApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
-      debugShowCheckedModeBanner: false,
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
