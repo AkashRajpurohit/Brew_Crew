400ea32a4851157c24661c019b513450b325624a
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-11-29 11:35:31 +0530

    :art: User model; serialize firebase user

---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class User {
+
+  final String uid;
+
+  User({ @required this.uid });
+
+}
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,21 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // Create user object based on firebase user
+  User _userFromFirbaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
   // Sign in anon
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirbaseUser(user);
     } catch(e) {
       print(e.toString());
       return null;
